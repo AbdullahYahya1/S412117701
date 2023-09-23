92fa347a9f1b2aa63ec1c8d41a97a9af15d95b97
AbdullahYahya1 <al7rag50@gmail.com>
2023-09-23 18:08:31 +0300

    a

--- index.php
+++ index.php
@@ -6,64 +6,138 @@
     <title>Document</title>
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
- 
-    <header>
-        <div class="container">
-            <h4>
-                Abdullah
-            </h4>
-            <p >
-                About Me 
-            </p>
-            <p>
-                Experience
-            </p>
-            <p>
-                Education
-            </p>
-            <p>
-                Certfifactions
-            </p>
-            <p>
-                Projects
-            </p>
-            <p>
-                Skills
-            </p>
-            <p>
-                Contact Me 
-            </p>
+        <div class='right_bar'>
+            <a href="#">
+                <div>
+                        <i class="fa-solid fa-house fa-xl"></i>          
+                </div>
+            </a>
+            <a href='#section2'>
+                <div>
+                    <i class="fa-solid fa-graduation-cap fa-xl"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-award fa-xl"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-code fa-xl"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-code fa-xl"></i>
+                </div>
+            </a>
+
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-address-card fa-xl"></i>
+                </div>
+            </a>
+
         </div>
-    </header>
+    <div class='Maincontainer'>
+
+        <header id='header'>
+            <div class="container">
+                <h4>
+                    Abdullah
+                </h4>
+                <p >
+                    About Me 
+                </p>
+                <p >
+                    Experience
+                </p>
+                <a href='#section2'>
+                <p >
+                    Education
+                </p>
+                </a>
+                <p>
+                    Certfifactions
+                </p>
+                <p>
+                    Projects
+                </p>
+                <p>
+                    Skills
+                </p>
+                <p>
+                    Contact Me 
+                </p>
+            </div>
+        </header>
 
         <img src="img1.png" class="img1">
         <div class="circle circle1"></div>
         <div class="circle circle2"></div>
-    <div class="section section1">
-        <div class="sec1C">
-            <p class='name'>
-                I'm Abdullah Yahya
-            </p>
-            <p class="job">
-                Problem Solver & Web Developer
-            </p>
-            <p class="interest">
-                I am intersted in solving any kind of problems
-                & I like learning new Stuff   
-            </p>
+        <div class="circle circle3"></div>
 
-            <div class="buttons_div">
-                <button> share Prtfolio</button>
-                <button> Download CV</button>
+
+        <div class="section section1"id='section1'>
+            <div class="sec1C">
+                <p class='name'>
+                    I'm Abdullah Yahya
+                </p>
+                <p class="job">
+                    Problem Solver & Web Developer
+                </p>
+                <p class="interest">
+                    I am intersted in solving any kind of problems
+                    & I like learning new Stuff   
+                </p>
+
+                <div class="buttons_div">
+                    <button> share Prtfolio</button>
+                    <button> Download CV</button>
+                </div>
             </div>
         </div>
+
+
+        <div class="sectionSep">
+                <hr>
+        </div>
+
+        <div class="section section2" id='section2'>
+            <div class="sec2C">
+                <h1 class='education'>
+                EDUCATION
+                </h1>
+                <div class="subeducation">
+                    <div class='edc-img'>
+                        <img class='edcimg' src="edcimg" alt="">
+                        <p class="edc1">Bachelor Computer-Science</p>
+                    </div>
+                    <p class="edc1-2">Qassim University</p>
+                    <p class="edc1-2">2020-Present</p>
+                </div>
+                <div class="subeducation">
+                    <div class='edc-img'>
+                        <img class='edcimg' src="edcimg" alt="">
+                        <p class="edc1">Bachelor Computer-Science</p>
+                    </div>
+                    <p class="edc1-2">Qassim University</p>
+                    <p class="edc1-2">2020-Present</p>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="sectionSep">
+                <hr>
+        </div>
+        <script src="app.js"></script>
     </div>
-    <div class="sectionSep">
-            <hr>
-    </div>
-    <script src="app.js"></script>
+
 </body>
 </html>
 
