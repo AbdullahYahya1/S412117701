b3c4c95e2b28a2f7ee742c3d00e36ad46ae3c31c
AbdullahYahya1 <al7rag50@gmail.com>
2023-09-23 13:21:50 +0300

    a

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+    <!-- <header>
+        <div class="container">
+            <h4>
+                Abdullah
+            </h4>
+            <p class="selected">
+                About Me 
+            </p>
+            <p>
+                Experience
+            </p>
+            <p>
+                Education
+            </p>
+            <p>
+                Certfifactions
+            </p>
+            <p>
+                Projects
+            </p>
+            <p>
+                Skills
+            </p>
+            <p>
+                Contact Me 
+            </p>
+        </div>
+    </header> -->
+        <img src="img1.png" class="img1">
+        <div class="circle circle1"></div>
+        <div class="circle circle2"></div>
+    <div class="section section1">
+        <div class="sec1C">
+            <p class='name'>
+                I'm Abdullah Yahya
+            </p>
+            <p class="job">
+                Problem Solver & Web Developer
+            </p>
+            <p class="interest">
+                I am intersted in solving any kind of problems
+                & I like learning new Stuff   
+            </p>
+
+            <div class="buttons_div">
+                <button> share Prtfolio</button>
+                <button> Download CV</button>
+            </div>
+        </div>
+    </div>
+    <div class="sectionSep">
+            <hr>
+    </div>
+    <script src="app.js"></script>
+</body>
+</html>
+
+
